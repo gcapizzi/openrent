68b978c6820f73eb3dba5bb196014642309252f3
Giuseppe Capizzi <g.capizzi@gmail.com>
2025-03-22 11:14:29 +0000

    Fix warnings

--- src/geo.rs
+++ src/geo.rs
@@ -1,5 +1,6 @@
 use anyhow::{anyhow, Result};
-use geo::{BoundingRect, Contains, Geometry, HaversineDistance};
+use geo::algorithm::line_measures::Haversine;
+use geo::{BoundingRect, Contains, Distance, Geometry, GeometryCollection};
 use serde::{Serialize, Serializer};
 
 pub struct Area {
@@ -29,21 +30,16 @@ impl Serialize for Point {
 }
 
 impl Area {
-    pub fn from_kml_file<P: AsRef<std::path::Path>>(path: P) -> Result<Area> {
-        let mut kml_reader = kml::KmlReader::<_, f64>::from_path(&path)?;
-        Ok(Area::from_kml_reader(&mut kml_reader)?)
-    }
-
     pub fn from_kml(string: String) -> Result<Area> {
         let mut kml_reader = kml::KmlReader::<_, f64>::from_string(&string);
-        Ok(Area::from_kml_reader(&mut kml_reader)?)
+        Area::from_kml_reader(&mut kml_reader)
     }
 
     fn from_kml_reader<B: std::io::BufRead>(
         kml_reader: &mut kml::KmlReader<B, f64>,
     ) -> Result<Area> {
         let kml_data = kml_reader.read()?;
-        let collection = kml::quick_collection(kml_data)?;
+        let collection = GeometryCollection::try_from(kml_data)?;
         let bounding_rectangle = collection
             .bounding_rect()
             .ok_or(anyhow!("no bounding rect"))?;
@@ -65,7 +61,7 @@ impl Area {
     pub fn radius(&self) -> u32 {
         let center = geo::Point::from(self.bounding_rectangle.center());
         let max = geo::Point::from(self.bounding_rectangle.max());
-        (center.haversine_distance(&max) / 1000.0).round() as u32
+        (Haversine::distance(center, max) / 1000.0).round() as u32
     }
 
     pub fn contains(&self, longitude: f64, latitude: f64) -> bool {
